f30ffe35ea24e4e3b77a765ff7a554b8f4f75301
helce <helce@bk.ru>
2023-12-19 11:13:50 +0300

    Replace every `String` in Target(Options) with `Cow<'static, str>`

--- compiler/rustc_target/src/spec/e2k64_unknown_linux_gnu.rs
+++ compiler/rustc_target/src/spec/e2k64_unknown_linux_gnu.rs
@@ -3,29 +3,29 @@ use crate::spec::{LinkArgs, LinkerFlavor, Target, TargetOptions};
 pub fn target() -> Target {
     let mut base = super::linux_gnu_base::opts();
     base.max_atomic_width = Some(64);
-    base.pre_link_args.entry(LinkerFlavor::Gcc).or_default().push("-fPIC".to_string());
+    base.pre_link_args.entry(LinkerFlavor::Gcc).or_default().push("-fPIC".into());
     let mut post_args = LinkArgs::new();
     post_args.insert(
         LinkerFlavor::Gcc,
         vec![
-            "-L/usr/lib/lccrt/lib/e2k64/".to_string(),
-            "-Wl,-rpath=/usr/lib/lccrt/lib/e2k64/".to_string(),
-            "-llccrt_s".to_string(),
-            "-llcc".to_string(),
-            "-lm".to_string(),
+            "-L/usr/lib/lccrt/lib/e2k64/".into(),
+            "-Wl,-rpath=/usr/lib/lccrt/lib/e2k64/".into(),
+            "-llccrt_s".into(),
+            "-llcc".into(),
+            "-lm".into(),
         ],
     );
     Target {
-        llvm_target: "e2k64-unknown-linux-gnu".to_string(),
+        llvm_target: "e2k64-unknown-linux-gnu".into(),
         pointer_width: 64,
-        data_layout: "e-m:e-p:64:64-i64:64:64-f80:128:128-n32:64-S128".to_string(),
-        arch: "e2k64".to_string(),
+        data_layout: "e-m:e-p:64:64-i64:64:64-f80:128:128-n32:64-S128".into(),
+        arch: "e2k64".into(),
 
         options: TargetOptions {
-            linker: Some(option_env!("ECC").unwrap_or("/usr/bin/gcc").to_string()),
+            linker: Some(option_env!("ECC").unwrap_or("/usr/bin/gcc").into()),
             position_independent_executables: false,
             post_link_args: post_args,
-            mcount: "\u{1}_mcount".to_string(),
+            mcount: "\u{1}_mcount".into(),
             ..base
         },
     }
--- compiler/rustc_target/src/spec/e2kv4_unknown_linux_gnu.rs
+++ compiler/rustc_target/src/spec/e2kv4_unknown_linux_gnu.rs
@@ -2,6 +2,6 @@ use crate::spec::Target;
 
 pub fn target() -> Target {
     let mut base = super::e2k64_unknown_linux_gnu::target();
-    base.options.cpu = "elbrus-v4".to_string();
+    base.options.cpu = "elbrus-v4".into();
     base
 }
