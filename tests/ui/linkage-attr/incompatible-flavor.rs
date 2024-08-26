e86cfab5e7040d2a83eabdc98cc397ea046fdd59
helce <helce@bk.ru>
2024-08-26 17:40:38 +0300

    disable x86-specific tests if llvm built without x86, part 3

--- tests/ui/linkage-attr/incompatible-flavor.rs
+++ tests/ui/linkage-attr/incompatible-flavor.rs
@@ -1,6 +1,6 @@
 //@ compile-flags: --target=x86_64-unknown-linux-gnu -C linker-flavor=msvc --crate-type=rlib
 //@ error-pattern: linker flavor `msvc` is incompatible with the current target
-//@ needs-llvm-components:
+//@ needs-llvm-components: x86
 
 #![feature(no_core)]
 #![no_core]
