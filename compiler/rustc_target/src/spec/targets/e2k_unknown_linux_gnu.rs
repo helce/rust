f3390af88106a3f27bf43a2475253b2ca8da27ec
helce <helce@bk.ru>
2025-04-25 20:50:48 +0300

    fill out target-spec metadata for e2k

--- compiler/rustc_target/src/spec/targets/e2k_unknown_linux_gnu.rs
+++ compiler/rustc_target/src/spec/targets/e2k_unknown_linux_gnu.rs
@@ -4,10 +4,10 @@ pub fn target() -> Target {
     Target {
         llvm_target: "e2k-unknown-linux-gnu".into(),
         metadata: crate::spec::TargetMetadata {
-            description: None,
-            tier: None,
-            host_tools: None,
-            std: None,
+            description: Some("64-bit Linux (kernel 5.10+, glibc 2.35+)".into()),
+            tier: Some(2),
+            host_tools: Some(false),
+            std: Some(true),
         },
         pointer_width: 64,
         data_layout: "e-m:e-p:64:64-i64:64-f64:64-f80:128:128-n32:64-S128".into(),
