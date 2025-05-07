37f3dec62d327880c0e2e5d706faabdb7cf48705
helce <helce@bk.ru>
2025-05-07 20:20:34 +0300

    disable fmas/fmad for e16c and generic v6

--- compiler/rustc_target/src/spec/targets/e2kv6_unknown_linux_gnu.rs
+++ compiler/rustc_target/src/spec/targets/e2kv6_unknown_linux_gnu.rs
@@ -1,7 +1,8 @@
-use crate::spec::Target;
+use crate::spec::{cvs, Target};
 
 pub fn target() -> Target {
     let mut base = super::e2k_unknown_linux_gnu::target();
     base.cpu = "elbrus-v6".into();
+    base.llvm_args = cvs!["-enable-elbrus-fma-legalization=0"];
     base
 }
