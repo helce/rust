1e3de18d83563cbf6c211a34ffc40a104039615f
helce <helce@bk.ru>
2025-05-28 11:47:19 +0300

    move to current interfaces

--- compiler/rustc_target/src/callconv/e2k.rs
+++ compiler/rustc_target/src/callconv/e2k.rs
@@ -1,5 +1,5 @@
-use crate::abi::call::{ArgAbi, FnAbi, Reg, Uniform};
-use crate::abi::{HasDataLayout, TyAbiInterface};
+use rustc_abi::{HasDataLayout, Reg, TyAbiInterface};
+use crate::callconv::{ArgAbi, FnAbi, Uniform};
 
 fn classify_ret<'a, Ty, C>(cx: &C, ret: &mut ArgAbi<'a, Ty>)
 where
