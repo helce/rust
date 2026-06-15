2270098743f09d8bf1281a6eabf21bfd579fb18b
helce <helce@bk.ru>
2026-06-15 20:16:43 +0300

    provide host tools only for e2s, e8c, e8c2 and e2c3

--- compiler/rustc_target/src/spec/targets/e2k2c3_unknown_linux_gnu.rs
+++ compiler/rustc_target/src/spec/targets/e2k2c3_unknown_linux_gnu.rs
@@ -3,5 +3,11 @@ use crate::spec::Target;
 pub(crate) fn target() -> Target {
     let mut base = super::e2k_unknown_linux_gnu::target();
     base.cpu = "elbrus-2c3".into();
+    base.metadata = crate::spec::TargetMetadata {
+        description: Some("64-bit Linux (kernel 5.10+, glibc 2.35+)".into()),
+        tier: Some(2),
+        host_tools: Some(true),
+        std: Some(true),
+    };
     base
 }
--- compiler/rustc_target/src/spec/targets/e2k_unknown_linux_gnu.rs
+++ compiler/rustc_target/src/spec/targets/e2k_unknown_linux_gnu.rs
@@ -6,7 +6,7 @@ pub(crate) fn target() -> Target {
         metadata: TargetMetadata {
             description: Some("64-bit Linux (kernel 5.10+, glibc 2.35+)".into()),
             tier: Some(2),
-            host_tools: Some(true),
+            host_tools: Some(false),
             std: Some(true),
         },
         pointer_width: 64,
